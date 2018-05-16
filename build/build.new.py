5885d2386995f9fc27fe21ddef4e353153e4d64a
kamiswee <ksweebe@nmsu.edu>
2018-05-16 16:20:43 -0600

    Uploading build application to Github
    Uploading build app.
    I kept admin.pyc in to see if it would be ignored.

---
+++ build/build.new.py
@@ -0,0 +1,24 @@
+# build.py
+# Author: @PieceOfKayk
+# 
+#
+# This script contains a build class.  The build class contains the following:
+#     
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
